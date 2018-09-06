7e4c88d09235ce0f2cbbe7dbba638437b7fb27a9
unknown <duytrung177@gmail.com>
2018-09-06 21:20:21 +0700

    up code Buoi 1 len git

---
+++ Session1/intro.php
@@ -0,0 +1,31 @@
+<?php
+
+    echo "Xin Chao hello ";
+    //Khai Bao Bien
+    echo '<br>';
+    $UserName = "php";
+    $a = 5;
+    
+    $b = 10;
+    
+    echo $a + $b;
+    
+    //Khai Bao Ham trong PHP
+    /* 
+     * Description: Gioi thieu chuc nang ham
+     * Created by: Nguoi tao
+     * Created Ngay Tao
+     */
+    function funtionName(){
+        
+    }
+    //Goi Ham
+    funtionName();
+    
+    function SumE($a, $b){
+        return $a + $b;
+    }
+    echo '<br>';
+    echo SumE(8, 4);
+    //Cau Dieu Kien
+?>
